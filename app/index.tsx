f8d1566a0aaa13f8a25e8ea6a584282f7de38f46
Nalinda Wickramarathna <mastersilicon@gmail.com>
2025-03-08 22:58:37 +0530

    nativewind setup

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-3xl text-dark-200 font-bold">Welcome back Nalinda 456.</Text>
+    </View>
+  );
+}
